05fd7dfbc61a14e2be9ab0414b230128ff3d29f1
Jerald Baroro <spaueOfficial@gmail.com>
2021-07-11 22:55:41 +0800

    Removed to test deployment

--- src/App.js
+++ src/App.js
@@ -21,13 +21,6 @@ export default function App() {
       <Router>
         <Suspense fallback={<p>Loading ...</p>}>
           <Switch>
-            {/* <IsUserLoggedIn user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.LOGIN}>
-              <Login />
-            </IsUserLoggedIn>
-            <IsUserLoggedIn user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.SIGN_UP}>
-              <SignUp />
-            </IsUserLoggedIn> */}
-
             <Route path={ROUTES.LOGIN} component={Login} />
             <Route path={ROUTES.SIGN_UP} component={SignUp} />
             <Route path={ROUTES.PROFILE} component={Profile} />
